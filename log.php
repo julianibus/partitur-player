8dccfa369b8a430abea874829014afbdf5a8c6c2
Julian Wienand <jwienand@aol.com>
2016-12-12 00:41:10 +0100

    update website, improve user tracking

--- log.php
+++ log.php
@@ -1,7 +1,7 @@
 <?php
 
-  $msg = $_GET["msg"];
-
-  file_put_contents("log.txt", $msg."\n", FILE_APPEND);
-
+	$msg = $_GET["msg"];
+	$file_data = $msg;
+	$file_data .= "\n".file_get_contents('log.txt');
+	file_put_contents('log.txt', $file_data);
 ?>
