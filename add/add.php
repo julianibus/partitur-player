b2361ec9905f4255e4026bc853588836a2eb3021
Julian Wienand <jwienand@aol.com>
2016-12-24 16:49:27 +0100

    work on adding process

--- add/add.php
+++ add/add.php
@@ -10,6 +10,9 @@
 <div id="form_container">
 <?php
 
+ob_implicit_flush(true);
+ob_end_flush();
+
 function flog($msg) {
 	echo $msg."<br>";
 	ob_flush();
@@ -27,29 +30,33 @@ function flogerror($msg) {
 	flush();
 }
 
+
 //Create unique jobid
-$jobid = rand(0,100000000);
+$jobid = date('Ymd-H-i-s');
 
 ##################### SECTION 1: FORM VALIDATION ##############################
-flog("Reading fields");
+flog("Reading fields...");
 
-$title = $_POST["title"];
-$ytcode = $_POST["ytcode"];
-$composer = $_POST["composer"];
-$begin = $_POST["begin"];
-$end = $_POST["end"];
-$source = $_POST["source"];
-$name = $_POST["name"];
-$email = $_POST["email"];
-$check = $_POST["check"];
-$opus = $_POST["opus"];
+$title = $_REQUEST["title"];
+$ytcode = $_REQUEST["ytcode"];
+$composer = $_REQUEST["composer"];
+$begin = $_REQUEST["begin"];
+$end = $_REQUEST["end"];
+$source = $_REQUEST["source"];
+$name = $_REQUEST["name"];
+$email = $_REQUEST["email"];
+$check = $_REQUEST["check"];
+$opus = $_REQUEST["opus"];
+$score = $_REQUEST["score"];
 
 ##################### SECTION 1: CREATE FILE STRUCTURE #######################
+flog("Creating file structure...");
 $folder = "../rep/".$jobid;
 $mediafolder = "../rep/".$jobid."/media";
 $pdf = "../rep/".$jobid."/media/pdf.pdf";
 $infofile = "../rep/".$jobid."/info.dat";
 $scorefile = "../rep/".$jobid."/score";
+$pdfimg = "../rep/".$jobid."/media/".$opus.".png";
 
 if (!file_exists($folder)) {
     mkdir($folder, 0777, true);
@@ -59,45 +66,59 @@ else {
 	flogerror("Error while creating file structure.");
 }
 
-##################### SECTION 3: FILE UPLOAD #################################
-flog("Uploading full score");
+##################### SECTION 3: FILE DOWNLOAD #################################
+flog("Downloading full score...");
 
-$target_dir = $mediafolder;
-$target_file = $pdf;
-$uploadOk = 1;
-$imageFileType = pathinfo($pdf,PATHINFO_EXTENSION);
-// Check if file already exists
-if (file_exists($target_file)) {
-    flogerror("Sorry, file already exists.");
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "pdf") {
-    flogerror("Wrong file type. The full score must be a pdf file.");
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["score"]["tmp_name"], $pdf)) {
-        echo "The file ". basename( $_FILES["score"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.".$_FILES["score"]["name"];
-    }
-}
+$url  = $score;
+$path = $pdf;
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$data = curl_exec($ch);
+
+curl_close($ch);
+
+file_put_contents($path, $data);
+
 ##################### SECTION 4: PDF TO  IMAGE CONVERSION ########################
+flogb("Processing PDF file...");
+$imagick = new Imagick();
+$imagick->readImage($pdf);
+//$imagick->writeImage($pdfimg, false);
+$imagick->setImageCompressionQuality(140);
+$num_pages = $imagick->getNumberImages();
+for($i = 0;$i < $num_pages; $i++) {         
+	flog("		Page ".$i);
 
+	// Set iterator postion
+	$imagick->setIteratorIndex($i);
 
-
+	// Write Images to temp 'upload' folder     
+	$imagick->writeImage("../rep/".$jobid."/media/".$opus."-".$i.".png");
+}
+flogb("Done.");
 
 ##################### SECTION 5: CREATE INFO FILE #################################
+
+$content = "";
+$content .= "title#".$title."\n";
+$content .= "composer#".$composer."\n";
+$content .= "score#".$source."\n";
+$content .= "score-url#".$source."\n";
+$content .= "music#".$ytcode."\n";
+$content .= "music-url#".$ytcode."\n";
+$content .= "code#".$ytcode."\n";
+$content .= "begin#".$begin."\n";
+$content .= "end#".$end."\n";
+$content .= "author#".$name."\n";
+$content .= "email#".$email."\n";
+$content .= "links#";
+
+file_put_contents($infofile, $content);
+
+$content2 = "0";
+file_put_contents($scorefile, $content2);
 
 
 
